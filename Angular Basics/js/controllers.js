0224fb6777454c45ea0abd653b456d89174c4ec3
BraZee <oddoyedavid@gmail.com>
2015-09-20 19:39:37 +0000

    Final App

--- Angular Basics/js/controllers.js
+++ Angular Basics/js/controllers.js
@@ -1,11 +1,27 @@
 angular.module('GDGLegon')
   .controller('AppController', function($scope, AppService) {
-    $scope.pageData = {
-      titleFromController: 'Title from Controller',
-      titleFromService: ''
-    };
 
-    $scope.showData = function(){
-      $scope.pageData.titleFromService = AppService.moreData();
+    //Initialise Application State
+    $scope.colourGenerated = false;
+    $scope.different = true;
+
+    //Submit function
+    $scope.submit = function(handle, colour) {
+      //Hand off validation to AppService
+      if (AppService.isValid(handle, colour)) {//If user entered a handle and colour, we're good to continue.
+        //Get Random Colour from AppService
+        $scope.generatedColor = AppService.getRandomColor();
+
+        //Check if random colour is the same as user colour
+        if ($scope.generatedColor.name === colour.toLowerCase()) {
+          //if same
+          $scope.different = false;
+        } else {
+          //if different
+          $scope.different = true;
+        }
+        //colour genration is done
+        $scope.colourGenerated = true;
+      }
     }
   });
